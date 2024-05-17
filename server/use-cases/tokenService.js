ae4a2f44b2b736c9e1a690ca70dc4d8a72d3ab76
BUENO Vincent <vbueno@enssat.fr>
2024-05-17 16:05:05 +0200

    Adding tokenService class

---
+++ server/use-cases/tokenService.js
@@ -0,0 +1,34 @@
+const { tokens, salon_id } = require('../../token/tokens_discord_bot.json');
+
+
+class TokenService extends Map{
+	constructor(){
+		super();
+		this.instance;
+	}
+
+    static getInstance(){ //Singleton
+        if(!this.instance){
+            this.instance = new TokenService();
+        }
+        return this.instance;
+    }
+
+    getToken(workerName){
+        if (tokens.length > 0){
+            var i = 0;
+            while (this.has(tokens[i]) && i < tokens.length){
+                i++;
+            }
+            if (i < tokens.length){
+                this.set(tokens[i],workerName);
+                return tokens[i];
+            }
+            else{
+                throw Error(`cannot get Token : no more token available. ${error} ${error.stack}`);
+            }
+        }
+    }
+}
+
+module.exports = TokenService;
